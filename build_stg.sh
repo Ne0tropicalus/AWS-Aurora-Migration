b712adaaf124beb2493e4cd2fd3b8165f8b7ff26
Ne0tropicalus <sean.hickey@slalom.com>
2019-06-27 17:53:21 +0000

    syncing with powerschool gi

--- build_stg.sh
+++ build_stg.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-
+############################################################################
+# build_stg.sh - runs after the daily automated aurora snapshots have run  #
+# requires:   awscli                                                       #
+# variables:  ic: instance cluster size to use for all databases           #
+#             az: availability zones for clusters                          #
+#             kms_key_id: string with the aws/kms key used for encryption  #
+#             subnet: subnet group to use in stage vpc                     #
+############################################################################
 export kms_key_id="arn:aws:kms:us-east-1:027444825683:key/a0896f07-0b13-4051-8ee3-eb188debe532"
 export subnet="prod-pm-student-subnet-grp"
 export az="us-east-1c us-east-1b us-east-1a"
 export ic="db.r5.large"
 export mobile=12066436491
+export err_cnt=0
+
+newrelic()
+{
+     local events=$1
+
+     echo ${events} | gzip -c -f | \
+     curl --data-binary @- -X POST -H "Content-Type: application/json" -H \
+        "X-Insert-Key: 0QhdzA_9TDU7RU8wUhTc_Imsd3yOI1GW" -H \
+	"Content-Encoding: gzip" \
+	https://insights-collector.newrelic.com/v1/accounts/1166112/events
+}
 
 delete_dbi () {
   local id=${1}
@@ -101,18 +120,44 @@ do
 
    ############ Delete the previous database instance ################
    delete_dbi ${id}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))" 
+   fi
    ############ Now delete the cluster - this also frees the older data ###########
    delete_dbc ${id}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))"
+   fi
    # The deletions take a while so will restore to a temporary set of names #
    ############ Create cluster from snapshot - this only creates a cluster container - stupid AWS :-( #####
    create_dbc ${id} ${snapshot}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))"
+   fi
    ############# Create db instance and add to cluster ###############
    create_dbi ${id}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))"
+   fi
    ############ Rename the cluster from tmp to STG ############
    rename_dbc ${id}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))"
+   fi
    ########### Rename the db instance from tmp to STG #########
    rename_dbi ${id}
+   if [ "$?" -eq "255" ]; then
+     err_cnt="$((err_cnt+1))"
+   fi
 done < aurora.list
-echo "--==={COMPLETED STG Environment}===--"
-aws sns publish --phone-number ${mobile} \
-    --message "--==={COMPLETED STG Environment `date`}===--" 
+if [ $err_cnt -gt 0 ]; then
+   echo "--==={FAILURE STG Environment: $err_cnt errors occured}===--"
+   aws sns publish --phone-number ${mobile} \
+       --message "--==={FAILURE STG Environment: $err_cnt errors occured}===--" 
+   newrelic """{"eventType":"stage-build","success":"F"}"""
+else
+    echo "--==={COMPLETED STG Environment}===--"
+    aws sns publish --phone-number ${mobile} \
+        --message "--==={COMPLETED STG Environment `date`}===--" 
+    newrelic """{"eventType":"stage-build","success":"T"}"""
+fi
